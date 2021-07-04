6c902349a3d3ef6f7e4f30c03a0495f56db7fb64
Joshua Chosay <jjchosay@gmail.com>
2021-07-04 09:29:20 -0500

    most functionality working. Linking remains.

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
 
-// TODO: Create an array of questions for user input
-const questions = [
+const generateMarkdown = (answers) =>
+`# ${answers.apptitle}
+
+## Table of Contents:
+1. Description
+2. Installation
+3. Usage
+4. License
+5. Contributing Guidelines
+6. Tests
+7. Questions
+
+### 1. Description
+
+${answers.description}
+
+### 2. Installation
+
+This application is web-deployed; users need not install anything.
+
+### 3. Usage
+
+You may use this software only if you are not a complete jibroni. If you don't understand what I mean by that, then you are included and may not use it.
+
+Ohio State fans are similarly barred from usage, as are in fact most Ohio residents.
+
+### 4. License
+
+This application is licensed by: ${answers.license}
+
+### 5. Contributing Guidelines
+
+Contributions are forbidden. If you try it, we will send people to your house to pulverize your lawn, let the air out of your kid's bike tires, and insult your spouse in grossly personal terms. You were warned.
+
+### 6. Tests
+Extensive testing has been done to ensure that this application works as intended; whaddya' think we are, crooks?
+
+### 7. Questions
+Any questions or comments may be directed to ${answers.fullname} at:
+Email: ${answers.email}
+GitHub: https://github.com/${answers.github}`
+
+inquirer.prompt([
     {
         type: 'input',
         message: 'What is your full name?',
@@ -35,59 +75,12 @@ const questions = [
         message: 'What is your GitHub user name?',
         name: 'github',
     }
-];
+])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-`#${apptitle}
+.then((answers) => {
+    const mdPageContent = generateMarkdown(answers);
 
-##Table of Contents:
-1. Description
-2. Installation
-3. Usage
-4. License
-5. Contributing Guidelines
-6. Tests
-7. Questions
-
-###1. Description
-${description}
-
-###2. Installation
-This application is web-deployed; users need not install anything.
-
-###3. Usage
-You may use this software only if you are not a complete jibroni. If you don't understand what I mean by that, then you are included and may not use it.
-
-Ohio State fans are similarly barred from usage, as are in fact most Ohio residents.
-
-###4. License
-This application is licensed by: ${license}
-
-###5. Contributing Guidelines
-Contributions are forbidden. If you try it, we will send people to your house to pulverize your lawn, let the air out of your kid's bike tires, and insult your spouse in grossly personal terms. You were warned.
-
-###6. Tests
-Extensive testing has been done to ensure that this application works as intended; whaddya' think we are, crooks?
-
-###7. Questions
-Any questions or comments may be directed to:
-${fullname}
-${email}
-https://github.com/${github}`
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt([questions])
-
-    .then((answers) => {
-        const mdPageContent = generateMarkdown(answers);
-
-        fs.writeFile('readme.md', mdPageContent, (err) =>
-            err ? console.log(err) : console.log('README file successfully generated. Thanks for playing.')
-        );
-    })
-}
-
-// Function call to initialize app
-init();
+    fs.writeFile('README.MD', mdPageContent, (err) =>
+        err ? console.log(err) : console.log('Successfully generated README.MD. Thanks for playing.') 
+    );
+});
